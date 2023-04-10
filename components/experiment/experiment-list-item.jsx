026464195659999c245bd63dd5c1c8b64b65b7a4
Felix Saaro <felix@saaro.ch>
2023-04-10 22:33:16 +0200

    Fix list item

--- components/experiment/experiment-list-item.jsx
+++ components/experiment/experiment-list-item.jsx
@@ -3,19 +3,17 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import { ListItem } from "../list/list-item";
 
 export const ExperimentListItem = ({ id, name, description }) => (
-    <ListItem className="p-0">
-        <>
-            <Link href={`/experiments/${id}`} className="flex justify-between items-center w-full p-6">
-                <div className="flex flex-1 flex-col gap-1">
-                    <div className="font-semibold truncate">
-                        {name}
-                    </div>
-                    <div className="truncate text-gray-500 text-xs">{description}</div>
+    <ListItem className="p-0 px-0 py-0">
+        <Link href={`/experiments/${id}`} className="flex justify-between items-center w-full p-6">
+            <div className="flex flex-1 flex-col gap-1">
+                <div className="font-semibold truncate">
+                    {name}
                 </div>
-                <div className="w-10 p-1 rounded-full text-gray-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <ArrowRightIcon className="block h-5 w-5" />
-                </div>
-            </Link>
-        </>
+                <div className="truncate text-gray-500 text-xs">{description}</div>
+            </div>
+            <div className="w-10 p-1 rounded-full text-gray-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                <ArrowRightIcon className="block h-5 w-5" />
+            </div>
+        </Link>
     </ListItem>
 )
