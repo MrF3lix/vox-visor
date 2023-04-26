0021ac4a64e64cd00303745da5d59eaf6b12cc9e
Felix Saaro <felix@saaro.ch>
2023-04-26 15:34:32 +0200

    Add infinite scroll

--- pages/runs/[id].jsx
+++ pages/runs/[id].jsx
@@ -1,15 +1,33 @@
 import Head from "next/head"
+import { useInView } from 'react-intersection-observer'
 import { List } from "../../components/list/list"
 import { ListItem } from "../../components/list/list-item"
 import { Private } from "../../components/private"
-import { useRun, useTranscripts } from "../../services/runs"
+import { fetchTranscripts, useRun } from "../../services/runs"
 import { RunDetail } from "../../components/run/run-detail"
 import { TranscriptListItem } from "../../components/transcript/transcript-list-item"
 import { Feed } from "../../components/feed/feed"
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { Fragment, useEffect } from "react"
 
 const Run = ({ id }) => {
+    const { ref, inView } = useInView()
     const { data: run } = useRun(id)
-    const { data: transcripts, isLoading } = useTranscripts(id)
+
+    const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage, } = useInfiniteQuery(
+        ['transcripts', id],
+        async ({ pageParam = 0 }) => await fetchTranscripts(id, pageParam),
+        {
+            getPreviousPageParam: (firstPage) => firstPage.previousId ?? undefined,
+            getNextPageParam: (lastPage) => lastPage.nextId ?? undefined,
+        },
+    )
+
+    useEffect(() => {
+        if (inView) {
+            fetchNextPage()
+        }
+    }, [inView])
 
     return (
         <Private>
@@ -38,18 +56,28 @@ const Run = ({ id }) => {
                 title="Transcripts"
                 description="List of transcripts created for each input audio file during this run."
             >
-                {!isLoading && transcripts.map(result => <TranscriptListItem key={result.id} {...result} />)}
-                {!isLoading && transcripts.length === 0 &&
-                    <ListItem>
-                        <div className="text-gray-500 text-xs">No results within this experiment found.</div>
-                    </ListItem>
-                }
-                {isLoading &&
+                {data && data.pages && data.pages.map(page => (
+                    <Fragment key={page.nextId}>
+                        {page?.data.map(result => <TranscriptListItem key={result.id} {...result} />)}
+                    </Fragment>
+                ))}
+                {isFetching &&
                     <ListItem>
                         <div className="text-gray-500 text-xs">Loading...</div>
                     </ListItem>
                 }
+                {!isFetching && data.pages && data.pages.length == 0 &&
+                    <ListItem>
+                        <div className="text-gray-500 text-xs">No results within this experiment found.</div>
+                    </ListItem>
+                }
             </List>
+            <button
+                ref={ref}
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+            >
+            </button>
         </Private>
     )
 }
--- services/runs.js
+++ services/runs.js
@@ -5,17 +5,13 @@ export const useRun = (id) => {
     return useQuery({ queryKey: ['run', id], queryFn: () => fetchRun(id) })
 }
 
-export const useTranscripts = (runId) => {
-    return useQuery({ queryKey: ['transcripts', runId], queryFn: () => fetchTranscripts(runId) })
-}
-
 const fetchRun = async (id) => {
     const { data, error } = await supabase
-    .from('run')
-    .select('id, name, description, createdAt: created_at, config')
-    .eq('id', id)
-    .order('created_at', { ascending: false })
-    .maybeSingle()
+        .from('run')
+        .select('id, name, description, createdAt: created_at, config')
+        .eq('id', id)
+        .order('created_at', { ascending: false })
+        .maybeSingle()
 
     if (error) {
         console.trace()
@@ -24,20 +20,25 @@ const fetchRun = async (id) => {
     }
 
     const stats = await fetchStats(id)
-    
+
     return {
         ...data,
         stats
     }
 }
 
-const fetchTranscripts = async (runId) => {
-    const {data} = await supabase.rpc('get_transcripts', {run_id: runId, offset_n: 0})
-    return data
+export const fetchTranscripts = async (runId, pageParam) => {
+    const { data } = await supabase.rpc('get_transcripts', { run_id: runId, offset_n: pageParam })
+    // await new Promise(r => setTimeout(r, 3000));
+    return {
+        data,
+        previousId: pageParam == 0 ? undefined : pageParam - 1,
+        nextId: pageParam + 1
+    }
 }
 
 export const saveRun = async (run) => {
-    const {data, error} = await supabase
+    const { data, error } = await supabase
         .from('run')
         .update({
             name: run.name,
@@ -55,6 +56,6 @@ export const saveRun = async (run) => {
 }
 
 const fetchStats = async (id) => {
-    const {data} = await supabase.rpc('get_run_stats', {run_id: id})
+    const { data } = await supabase.rpc('get_run_stats', { run_id: id })
     return data[0]
 }
